70b06450d3e391d13c8429840980531295c7f239
Bhabajit <Bhabajit@192.168.2.8>
2023-09-27 20:20:20 +0530

    Hii fuckers

---
+++ Allfeature/Feature.feature
@@ -0,0 +1,5 @@
+Feature:Test orangeHRM application
+Scenario: Test login
+Given user on login page
+When user enter valid user name and password
+Then user on Homepage
---
+++ src/test/java/TestRunner/alltestrunner.java
@@ -0,0 +1,24 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features= {"Allfeature"},
+glue= {"Stepdefination"},
+dryRun=false
+)
+public class alltestrunner {
+	
+			
+
+	        
+   
+	
+	}
+
+
+
+
